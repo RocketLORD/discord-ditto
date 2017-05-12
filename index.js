8f616b4640d2948a39adf15024e38afd9ece71b3
Janku-Roketto <joshua.f.reade@gmail.com>
2017-05-12 23:40:58 +0100

    transform command

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-// WEBPAGE
+
+// INIT
+var Discordie = require("discordie");
+var client = new Discordie({autoReconnect: true});
 
 var express = require('express');
 var app = express();
 
+//
+// DISCORD
+//
+client.connect({
+  token: process.env.BOT_SECRET
+});
+
+client.Dispatcher.on("GATEWAY_READY", e => {
+  console.log("Connected as: " + client.User.username);
+});
+
+client.Dispatcher.on("MESSAGE_CREATE", e => {
+  if (e.message.content == "ping")
+    e.message.channel.sendMessage("pong");
+
+  if (e.message.content == "transform bulbasaur")
+	client.User.setAvatar(fs.readFileSync("/public/dd_bulbasaur.jpg");
+
+  if (e.message.content == "transform ditto")
+	client.User.setAvatar(fs.readFileSync("/public/dd_bulbasaur.jpg");
+});
+
+//
+// WEBPAGE
+//
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -18,20 +46,3 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-// DISCORD
-
-var Discordie = require("discordie");
-var client = new Discordie({autoReconnect: true});
-
-client.connect({
-  token: process.env.BOT_SECRET
-});
-
-client.Dispatcher.on("GATEWAY_READY", e => {
-  console.log("Connected as: " + client.User.username);
-});
-
-client.Dispatcher.on("MESSAGE_CREATE", e => {
-  if (e.message.content == "ping")
-    e.message.channel.sendMessage("pong");
-});
