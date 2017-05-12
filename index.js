d36986e77779925e231fbce4a6cd3a176fb7708a
Janku-Roketto <joshua.f.reade@gmail.com>
2017-05-12 23:10:29 +0100

    web commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+// DISCORD
+
 var Discordie = require("discordie");
 var client = new Discordie();
 
 client.connect({
-  // replace this sample token
   token: process.env.BOT_SECRET
 });
 
@@ -13,4 +14,24 @@ client.Dispatcher.on("GATEWAY_READY", e => {
 client.Dispatcher.on("MESSAGE_CREATE", e => {
   if (e.message.content == "ping")
     e.message.channel.sendMessage("pong");
+});
+
+// WEBPAGE
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
