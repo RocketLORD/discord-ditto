a53ba53e1711b5f07b24662907f65dae1a97f5f6
Janku-Roketto <joshua.f.reade@gmail.com>
2017-05-13 00:00:12 +0100

    fixed transform command flopping

--- index.js
+++ index.js
@@ -3,30 +3,10 @@
 var Discordie = require("discordie");
 var client = new Discordie({autoReconnect: true});
 
+var fs = require('file-system');
+
 var express = require('express');
 var app = express();
-
-//
-// DISCORD
-//
-client.connect({
-  token: process.env.BOT_SECRET
-});
-
-client.Dispatcher.on("GATEWAY_READY", e => {
-  console.log("Connected as: " + client.User.username);
-});
-
-client.Dispatcher.on("MESSAGE_CREATE", e => {
-  if (e.message.content == "ping")
-    e.message.channel.sendMessage("pong");
-
-  if (e.message.content == "transform bulbasaur")
-	client.User.setAvatar(fs.readFileSync("/public/dd_bulbasaur.jpg"));
-
-  if (e.message.content == "transform ditto")
-	client.User.setAvatar(fs.readFileSync("/public/dd_logo.jpg"));
-});
 
 //
 // WEBPAGE
@@ -46,3 +26,26 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+
+
+//
+// DISCORD
+//
+client.connect({
+  token: process.env.BOT_SECRET
+});
+
+client.Dispatcher.on("GATEWAY_READY", e => {
+  console.log("Connected as: " + client.User.username);
+});
+
+client.Dispatcher.on("MESSAGE_CREATE", e => {
+  if (e.message.content == "ping")
+    e.message.channel.sendMessage("pong");
+
+  if (e.message.content == "transform bulbasaur")
+	client.User.setAvatar(fs.readFileSync(__dirname + "/public/dd_bulbasaur.jpg"));
+
+  if (e.message.content == "transform ditto")
+	client.User.setAvatar(fs.readFileSync(__dirname + "/public/dd_ditto.jpg"));
+});
